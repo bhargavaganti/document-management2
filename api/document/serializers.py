0326cbfdb8bcb65430339fab62b1fe7b4becb022
Serban <serban.dobroiu@gmail.com>
2018-04-22 10:02:56 +0000

    quit

--- api/document/serializers.py
+++ api/document/serializers.py
@@ -3,6 +3,8 @@ from rest_framework import serializers
 
 # Document serializer 
 class DocumentSerializer(serializers.ModelSerializer):
+    owner = serializers.PrimaryKeyRelatedField(many = False, read_only = False, queryset = Customer.objects.all())
+    package = serializers.PrimaryKeyRelatedField(many = False, read_only = False, queryset = Package.objects.all())
     class Meta:
         model = Document
         fields = ('__all__')
@@ -24,8 +26,7 @@ class CustomerSerializer(serializers.ModelSerializer):
     customer_packages = PackageSerializer(many = True)
     class Meta:
         model = Customer
-        fields = ('customer_id', 'first_name', 'last_name', 'title', 'email', 
-        'created_by', 'created_at', 'modified_by','customer_packages')
+        fields = ('__all__')
         read_only_fields = ('created_by', 'created_at', 'modified_by', 'modified_at')
         lokup_value = 'customer_id'
 
